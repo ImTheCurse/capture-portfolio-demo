0723283b1954f3816472c26829b51f1e1ffb93c2
ImTheCurse <33231841+ImTheCurse@users.noreply.github.com>
2023-08-09 13:25:27 +0300

    Added ServiceSection

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,15 @@
 import React from "react";
 import AboutSection from "../comp/AboutSection";
+import ServicesSection from "../comp/ServicesSection";
 
 function AboutUs(){
     return (
+        
+        <>
         <AboutSection/>
-    )
+        <ServicesSection/>
+        </>
+    );
 }
 
 export default AboutUs;
